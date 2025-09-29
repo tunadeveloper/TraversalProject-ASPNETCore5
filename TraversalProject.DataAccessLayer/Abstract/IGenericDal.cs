5dc137861f1ab82ae44d28147eb63b9a0b69f86b
tunadeveloper <tunabusiness25@gmail.com>
2025-09-29 15:20:26 +0300

    Updated

---
+++ TraversalProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraversalProject.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T p);
+        void Delete(T p);
+        void Update(T p);
+        List<T> GetAll();
+        T GetById(int id);
+    }
+}
---
+++ TraversalProject.DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraversalProject.DataAccessLayer.Abstract;
+using TraversalProject.DataAccessLayer.Concrete;
+
+namespace TraversalProject.DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T p)
+        {
+            _context.Remove(p);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+           return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T p)
+        {
+           _context.Add(p);
+            _context.SaveChanges();
+        }
+
+        public void Update(T p)
+        {
+           _context.Update(p);
+            _context.SaveChanges();
+        }
+    }
+}
