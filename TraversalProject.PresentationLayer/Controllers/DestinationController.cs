1f4e0dc7c3f87f027033427f2bfe4db75e8e58ce
tunadeveloper <tunabusiness25@gmail.com>
2025-09-30 10:49:59 +0300

    Updated

---
+++ TraversalProject.PresentationLayer/Controllers/DestinationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TraversalProject.BusinessLayer.Abstract;
+
+namespace TraversalProject.PresentationLayer.Controllers
+{
+    public class DestinationController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public DestinationController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _destinationService.GetListBL();
+            return View(values);
+        }
+
+        public IActionResult DestinationDetails(int id)
+        {
+            var values = _destinationService.GetByIdBL(id);
+            return View(values);
+        }
+    }
+}
