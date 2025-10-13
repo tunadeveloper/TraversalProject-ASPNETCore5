ff9deb17ae3c3ee08738fafb90e0093b986d708f
tunadeveloper <tunabusiness25@gmail.com>
2025-10-13 21:56:07 +0300

    Updated

---
+++ TraversalProject.PresentationLayer/Areas/Member/Models/UserEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TraversalProject.PresentationLayer.Areas.Member.Models
+{
+    public class UserEditViewModel
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public string imageUrl { get; set; }
+
+    }
+}
