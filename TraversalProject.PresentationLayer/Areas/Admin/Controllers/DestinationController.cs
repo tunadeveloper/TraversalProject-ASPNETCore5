1ae9b35a628f62d80dabdf97ba927f32dd2a90f1
Tunahan Cengiz <140824629+tunadeveloper@users.noreply.github.com>
2025-10-28 17:41:17 +0300

    Updated

---
+++ TraversalProject.PresentationLayer/Areas/Admin/Controllers/DestinationController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TraversalProject.BusinessLayer.Abstract;
+using TraversalProject.EntityLayer.Concrete;
+
+namespace TraversalProject.PresentationLayer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DestinationController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public DestinationController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _destinationService.GetListBL();
+            return View(values);
+        }
+
+        public IActionResult CreateDestination() => View();
+
+        [HttpPost]
+        public IActionResult CreateDestination(Destination destination)
+        {
+            _destinationService.InsertBL(destination);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteDestination(int id)
+        {
+            var destinationValue = _destinationService.GetByIdBL(id);
+            _destinationService.DeleteBL(destinationValue);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UpdateDestination (int id)
+        {
+            var destinationValue = _destinationService.GetByIdBL(id);
+            return View(destinationValue);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            _destinationService.UpdateBL(destination);
+            return RedirectToAction("Index");
+        }
+}
+}
