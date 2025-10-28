fbeb6e35745719a8f632927b48b6c67c4a627874
Tunahan Cengiz <140824629+tunadeveloper@users.noreply.github.com>
2025-10-28 17:04:23 +0300

    Updated

---
+++ TraversalProject.PresentationLayer/ViewComponents/MemberDashboard/_PlatformSettingComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalProject.PresentationLayer.ViewComponents.MemberDashboard
+{
+    public class _PlatformSettingComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+        }
+}
