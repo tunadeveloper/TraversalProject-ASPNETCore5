4e68643a255362d3499a51fb02540b153a3a981c
Tunahan Cengiz <140824629+tunadeveloper@users.noreply.github.com>
2025-11-01 20:12:38 +0300

    Updated

---
+++ TraversalProject.PresentationLayer/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TraversalProject.BusinessLayer.Abstract;
+
+namespace TraversalProject.PresentationLayer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalProject.PresentationLayer/Controllers/ErrorPage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalProject.PresentationLayer.Controllers
+{
+    public class ErrorPage : Controller
+    {
+        public IActionResult Error404()
+        {
+            return View();
+        }
+    }
+}
