dd85f0b8d98329a7a5f9159dc518c65c615ff9ad
Tunahan Cengiz <140824629+tunadeveloper@users.noreply.github.com>
2025-11-02 18:08:57 +0300

    Updated

--- TraversalProject.PresentationLayer/Controllers/ExcelController.cs
+++ TraversalProject.PresentationLayer/Controllers/ExcelController.cs
@@ -1,22 +1,94 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
+using System.Collections.Generic;
+using System.Linq;
+using TraversalProject.BusinessLayer.Abstract;
+using TraversalProject.DataAccessLayer.Concrete;
+using TraversalProject.PresentationLayer.Models;
 
 namespace TraversalProject.PresentationLayer.Controllers
 {
+    [AllowAnonymous]
     public class ExcelController : Controller
+
     {
         public IActionResult Index()
         {
-            ExcelPackage excel = new ExcelPackage();
+            return View();
+        }
+
+        public List<DestinationModel> DestinationList()
+        {
+            List<DestinationModel> destinationModels = new List<DestinationModel>();
+            using (var context = new Context())
+            {
+                destinationModels = context.Destinations.Select(x => new DestinationModel
+                {
+                    City = x.City,
+                    DayNight = x.DayNight,
+                    Price = x.Price,
+                    Capacity = x.Capacity
+                }).ToList();
+            }
+            return destinationModels;
+        }
+
+        public IActionResult StaticExcelReport()
+        {
+            ExcelPackage.License.SetNonCommercialPersonal("Tunahan Cengiz");
+
+            using var excel = new ExcelPackage();
             var workSheet = excel.Workbook.Worksheets.Add("Deneme1");
+
             workSheet.Cells[1, 1].Value = "Rota";
             workSheet.Cells[1, 2].Value = "Rehber";
-            workSheet.Cells[1, 1].Value = "Kontenjan";
+            workSheet.Cells[1, 3].Value = "Kontenjan";
 
             workSheet.Cells[2, 1].Value = "Gürcistan Batum";
             workSheet.Cells[2, 2].Value = "Tunahan Cengiz";
             workSheet.Cells[2, 3].Value = "50";
-            return View();
+
+            var bytes = excel.GetAsByteArray();
+            return File(bytes,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "Deneme1.xlsx");
         }
+
+        #region DestinationExcelReport
+        public IActionResult DestinationExcelReport()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Tur Listesi");
+                worksheet.Cell(1, 1).Value = "Şehir";
+                worksheet.Cell(1, 2).Value = "Konaklama Süresi";
+                worksheet.Cell(1, 3).Value = "Fiyat";
+                worksheet.Cell(1, 4).Value = "Kapasite";
+
+                int rowCount = 2;
+                foreach (var item in DestinationList())
+                {
+                    worksheet.Cell(rowCount, 1).Value = item.City;
+                    worksheet.Cell(rowCount, 2).Value = item.DayNight;
+                    worksheet.Cell(rowCount, 3).Value = item.Price;
+                    worksheet.Cell(rowCount, 4).Value = item.Capacity;
+                    rowCount++;
+                }
+
+                using (var stream = new System.IO.MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content,
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        "TurListesi.xlsx");
+                }
+            }
+        }
+        #endregion
     }
 }
+
+
