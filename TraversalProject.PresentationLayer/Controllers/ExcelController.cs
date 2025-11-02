c9f86729b5bde7c7c84ab5a3868ef583bdddcc3f
Tunahan Cengiz <140824629+tunadeveloper@users.noreply.github.com>
2025-11-02 17:47:47 +0300

    Updated

---
+++ TraversalProject.PresentationLayer/Controllers/ExcelController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+namespace TraversalProject.PresentationLayer.Controllers
+{
+    public class ExcelController : Controller
+    {
+        public IActionResult Index()
+        {
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Deneme1");
+            workSheet.Cells[1, 1].Value = "Rota";
+            workSheet.Cells[1, 2].Value = "Rehber";
+            workSheet.Cells[1, 1].Value = "Kontenjan";
+
+            workSheet.Cells[2, 1].Value = "Gürcistan Batum";
+            workSheet.Cells[2, 2].Value = "Tunahan Cengiz";
+            workSheet.Cells[2, 3].Value = "50";
+            return View();
+        }
+    }
+}
