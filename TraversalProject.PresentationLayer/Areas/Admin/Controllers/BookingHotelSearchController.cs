19adebe59783068f9a67591db9f94ab430b98796
Tunahan Cengiz <tunahancengizyt250@gmail.com>
2025-12-19 21:07:06 +0300

    Updated

---
+++ TraversalProject.PresentationLayer/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TraversalProject.PresentationLayer.Areas.Admin.Models;
+
+namespace TraversalProject.PresentationLayer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class BookingHotelSearchController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            BookingHotelSearchViewModel model;
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(
+                    "https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels?dest_id=-2092174&search_type=CITY&arrival_date=2025-12-20&departure_date=2025-12-25&adults=1&room_qty=1&currency_code=TRY"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "e71177d867mshfa37763bf5e2e10p1b4212jsn34b1dc08d068" },
+                    { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+                },
+            };
+
+            using var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            model = JsonSerializer.Deserialize<BookingHotelSearchViewModel>(
+                body,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+            return View(model);
+        }
+}
+}
