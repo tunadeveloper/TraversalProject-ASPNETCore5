0be38b35161a6e7502e0d5dbedfd2d40e4da2161
tunadeveloper <tunabusiness25@gmail.com>
2025-12-27 22:28:51 +0300

    Updated

--- TraversalProject.DataAccessLayer/Concrete/Context.cs
+++ TraversalProject.DataAccessLayer/Concrete/Context.cs
@@ -28,5 +28,6 @@ namespace TraversalProject.DataAccessLayer.Concrete
         public DbSet<Reservation> Reservations { get; set; }
         public DbSet<ContactUs> ContactUses { get; set; }
         public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Account> Accounts { get; set; }
     }
 }
